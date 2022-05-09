0036130836308c4e32e650c6bee201eafe95c71c
akbsh <99663712+akbsh@users.noreply.github.com>
2022-05-09 10:24:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ RBC= cv2.imread('RBC.jpg')
 gray = cv2.cvtColor(RBC, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap='gray');
 
-#in order to detect images we need to turn it into blur, this way we can avoid the noise
-#the 9 9 is kernel, hover over whole picture, 0 is stdv
-#the more you increase the size of kernel the more blured it becomes
+#in order to detect cells, we need to turn the image into blur, this way we can avoid the noise around
+#i used Gaussianblur method
+#the 9 x 9 is kernel, that hover over whole picture, 0 is stdv
+#the more you increase the size of entered kernel the more blured it becomes
 Gaussianblur = cv2.GaussianBlur(gray, (9,9), 0)
 plt.imshow(Gaussianblur, cmap='gray');
 
-#for detecting the edges Cany edge algorithm can be used in this case
+#for detecting the edges of cells Cany edge algorithm can be used in this case
 # the blurred image should be used to avoid the noise around the desired round circles
 #the 3 number is default, it is for finding edges
-#lower and upper values are set to identify the edges we can adjust it for our picture
+#lower and upper values are set to identify the edges, we can adjust it for our picture, with best adjustment the right number of cell can be found
 contour = cv2.Canny(Gaussianblur, 70, 160, 3)
 plt.imshow(contour, cmap='gray')
 
@@ -30,17 +31,18 @@ plt.imshow(str_con, cmap='gray')
 
 #counting
 #cv2.retr_external means counting only external edges
-#cv2.chain_approx_none considering all contours
-(cnt, heirarchy) = cv2.findContours(str_con.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)\
+#cv2.chain_approx_none means considering all contours
+(cnt, heirarchy) = cv2.findContours(str_con.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+#here i want to contour with blue cells in original image
 #convertation of bgr to rgb matplot reads in brg, original is rgb
 RGB = cv2.cvtColor(RBC, cv2.COLOR_BGR2RGB)
-#first is our RGB image, then cnt is contours, -1 means all contours, (0,0,255) means blue, the last is how thick
+#first is our RGB image, then cnt is contours, -1 means for all contours, (0,0,255) means blue, the last is how thick
 cv2.drawContours(RGB, cnt, -1, (0, 0, 255), 1)
 
 #final picture
 plt.imshow(RGB)
 
-#counting
+#printing how much cells were counted
 print('Round cells in the image: ', len(cnt))
 plt.show()
